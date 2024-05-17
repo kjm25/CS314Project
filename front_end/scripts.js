df6b18a06df534041b438104a94e6f5188ef64f2
Kevin Macdonald <kjm25@cecs.pdx.edu>
2024-05-17 12:18:12 -0700

    Google now prompts only if you are not signed in

--- front_end/scripts.js
+++ front_end/scripts.js
@@ -102,6 +102,7 @@ function save_cookie(response, exp)
 {
     response_string = JSON.stringify(response);
     document.cookie = "id_token=" + response_string + "; expires=" + new Date(exp * 1000).toUTCString() + "; path=/";
+    document.cookie = "sid=" + "Logged In" + "; expires=" + new Date(exp * 1000).toUTCString() + "; path=/";
     console.log("id_token=" + response_string + "; expires=" + new Date(exp * 1000).toUTCString() + "; path=/");
 }
 
