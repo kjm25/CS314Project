9fe4fdb5881ffc6feab6038f46dcf28359744938
Will Schweitzer <willSchweitzer@yahoo.com>
2024-05-29 14:47:09 -0700

    Fixed welcome message not being aligned (on certain screens)

--- client/index.js
+++ client/index.js
@@ -64,8 +64,8 @@ function WelcomePage()
   return (
     <div className="bg-dark welcome-page">
       <div className="welcome-text">
-        <h1 className="text-light text-center">Welcome to Chaterize!</h1>
-        <h2 className="text-light text-center">Please sign-in with Google to start chating.</h2>
+        <h1 className="">Welcome to Chaterize!</h1>
+        <h2 className="">Please sign-in with Google to start chating.</h2>
       </div>
     </div>
   )
