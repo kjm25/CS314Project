6455c1afff6e17c2f814f21cc11965b783062c80
Will Schweitzer <willSchweitzer@yahoo.com>
2024-05-30 09:11:30 -0700

    Moved things around so that the Date component has everything it needs between Date.js and Date.css.  Also added a client_id constant

--- client_react/src/components/Constants.js
+++ client_react/src/components/Constants.js
@@ -7,3 +7,5 @@ export const CLIENT_EMIT_DELETE_CHAT = 'delete_chat';
 export const DEBUGGING = true;
 
 export const GREETINGS = ["Hello", "Hi", "Sup"]
+
+export const CLIENT_ID = '278406872967-ds0j19p8s6gouvvklrma8cmpjicpmnfu.apps.googleusercontent.com'
