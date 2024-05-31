2f706d7ffbb0606b940e1a8f7a55e4e46bf54e66
Kevin Macdonald <kjm25@cecs.pdx.edu>
2024-05-31 14:56:14 -0700

    test app rendering

--- client_react/src/App.test.js
+++ client_react/src/App.test.js
@@ -1,9 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import DateTime from './components/Date';
+const socket_client = require('socket.io-client');
+
+
+beforeAll(async () => { //before tests starts connect to MongoDB
+  window.globalsocket = socket_client(); //gives mock socket for app - emits some erros about not finding server
+ // await client.connect();
+  //db = client.db('testDB');
+});
 
 test('renders learn react link', () => {
   render(<DateTime datetime={0}/>);
-  expect(1).toBe(1);
+  expect(3).toBe(3);
   //const linkElement = screen.getByText(/learn react/i);
 });
+
+describe("The entire app renders", () => {
+  test('The entire app renders with welcome page', () => {
+    document.cookie = "id_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    render(<App />);
+  });
+
+  test('The entire app renders with main page', () => {
+    document.cookie = "id_token=Notwelcome; Max-Age=50; path=/;";
+    render(<App />);
+  });
+}); 
+
