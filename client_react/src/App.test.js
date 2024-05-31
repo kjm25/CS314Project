95b784c4caddcdca4370d25054bd683b0eab216a
Kevin Macdonald <kjm25@cecs.pdx.edu>
2024-05-31 14:40:22 -0700

    framework for react testing [skip render]

--- client_react/src/App.test.js
+++ client_react/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import DateTime from './components/Date';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<DateTime datetime={0}/>);
+  expect(1).toBe(1);
+  //const linkElement = screen.getByText(/learn react/i);
 });
